106e947f0179f5da622a20a894d9e339d7dd9f6c
RobJWong <RobertJWong246@gmail.com>
2018-05-04 23:37:42 -0400

    barcode reader

--- BarcodeReader/BarcodeReader/ViewController.swift
+++ BarcodeReader/BarcodeReader/ViewController.swift
@@ -13,53 +13,82 @@ class ViewController: UIViewController, AVCaptureMetadataOutputObjectsDelegate {
     
     @IBOutlet weak var square: UIImageView!
     
+    var qrCodeFrameView: UIView?
     var video = AVCaptureVideoPreviewLayer()
+    var session = AVCaptureSession()
+    var supportedCodeTypes = [AVMetadataObject.ObjectType.upce,
+                              AVMetadataObject.ObjectType.code39,
+                              AVMetadataObject.ObjectType.code39Mod43,
+                              AVMetadataObject.ObjectType.code93,
+                              AVMetadataObject.ObjectType.code128,
+                              AVMetadataObject.ObjectType.ean8,
+                              AVMetadataObject.ObjectType.ean13]
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        
-        let session = AVCaptureSession()
         
         let captureDevice = AVCaptureDevice.default(for: .video)
         
         do {
             let input = try AVCaptureDeviceInput(device: captureDevice!)
             session.addInput(input)
+            let output = AVCaptureMetadataOutput()
+            session.addOutput(output)
+            output.setMetadataObjectsDelegate(self, queue: DispatchQueue.main)
+            output.metadataObjectTypes = supportedCodeTypes
         }
         catch {
             print("error")
         }
-        
-        let output = AVCaptureMetadataOutput()
-        session.addOutput(output)
-        
-        output.setMetadataObjectsDelegate(self, queue: DispatchQueue.main)
-        
-        output.metadataObjectTypes = [AVMetadataObject.ObjectType.ean13]
         
         video = AVCaptureVideoPreviewLayer(session: session)
         video.frame = view.layer.bounds
         view.layer.addSublayer(video)
         self.view.bringSubview(toFront: square)
         session.startRunning()
+        
+        qrCodeFrameView = UIView()
+        if let qrCodeFrameView = qrCodeFrameView {
+            qrCodeFrameView.layer.borderColor = UIColor.green.cgColor
+            qrCodeFrameView.layer.borderWidth = 2
+            view.addSubview(qrCodeFrameView)
+            view.bringSubview(toFront: qrCodeFrameView)
+        }
     }
     
+//    func metadataOutput(_ output: AVCaptureMetadataOutput, didOutput metadataObjects: [AVMetadataObject], from connection: AVCaptureConnection) {
+//        if metadataObjects != nil && metadataObjects.count != 0
+//        {
+//            if let object = metadataObjects[0] as? AVMetadataMachineReadableCodeObject
+//            {
+//                if object.type == AVMetadataObject.ObjectType.upce
+//                {
+//                    print(object.stringValue)
+//                }
+//            }
+//        }
+//    }
+    
     func metadataOutput(_ output: AVCaptureMetadataOutput, didOutput metadataObjects: [AVMetadataObject], from connection: AVCaptureConnection) {
-        if metadataObjects != nil && metadataObjects.count != 0
-        {
-            if let object = metadataObjects[0] as? AVMetadataMachineReadableCodeObject
-            {
-                if object.type == AVMetadataObject.ObjectType.upce
-                {
-                    print(object.stringValue)
-//                    let alert = UIAlertController(title: "Barcode", message: object.stringValue, preferredStyle: .alert)
-//                    alert.addAction(UIAlertAction(title: "Retake", style: .default, handler: nil))
-//                    alert.addAction(UIAlertAction(title: "Copy", style: .default, handler: { (nil) in
-//                        UIPasteboard.general.string = object.stringValue
-//                    }))
-//                    present(alert, animated: true, completion: nil)
-                }
+        if metadataObjects.count == 0 {
+            qrCodeFrameView?.frame = CGRect.zero
+            //messageLabel.text = "No QR code is detected"
+            print("nothing")
+            return
+        }
+        
+        let metadataObj = metadataObjects[0] as! AVMetadataMachineReadableCodeObject
+        
+        if supportedCodeTypes.contains(metadataObj.type) {
+            // If the found metadata is equal to the QR code metadata (or barcode) then update the status label's text and set the bounds
+            let barCodeObject = video.transformedMetadataObject(for: metadataObj)
+            qrCodeFrameView?.frame = barCodeObject!.bounds
+            
+            if metadataObj.stringValue != nil {
+                //launchApp(decodedURL: metadataObj.stringValue!)
+                //messageLabel.text = metadataObj.stringValue
+                print(metadataObj.stringValue)
             }
         }
     }
